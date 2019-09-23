197a113849e540de986a457496510356df5920b7
Sean Yue <sean.yue@skylinetw.com>
2019-09-23 17:17:40 +0800

    ProductTableViewCell configuration

---
+++ CoordinatorDemo/Main/View/ProductTableViewCell/ProductTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProductTableViewCell.swift
+//  CoordinatorDemo
+//
+//  Created by Sean.Yue on 2019/9/21.
+//  Copyright © 2019 Sean.Yue. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
